0e561460db79df1da893e60d465145c95a983d94
Mohamed Abdelaziz Louhab <86206955+medAzizLHB@users.noreply.github.com>
2021-12-10 18:27:48 +0100

    pyramids and triangles

---
+++ reverse sentence.c
@@ -0,0 +1,20 @@
+/*
+* medAzizLHB
+* SkorpTech
+*/
+#include <stdio.h>
+void reverseSentence();
+int main() {
+    printf("Enter a sentence: ");
+    reverseSentence();
+    return 0;
+}
+
+void reverseSentence() {
+    char c;
+    scanf("%c", &c);
+    if (c != '\n') {
+        reverseSentence();
+        printf("%c", c);
+    }
+}
